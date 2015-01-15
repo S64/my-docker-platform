8a6094f701ded8ff613d4e5f6ccc8d5f4c0d0734
Shuma Yoshioka <s64.stdio@gmail.com>
2015-01-16 07:07:44 +0900

    Update

--- install.sh
+++ install.sh
@@ -17,14 +17,14 @@ if [ -z ${DNSMASQ_CONFIG_DIR} ]; then
   DNSMASQ_CONFIG_DIR=$CURRENT"/_dnsmasq/example_dnsmasq.d"
 fi
 
-if [ -z ${AIPO_BACKUP_DIR} ]; then
-  AIPO_BACKUP_DIR="/var/tmp/aipo/backup"
-fi
+#if [ -z ${AIPO_BACKUP_DIR} ]; then
+#  AIPO_BACKUP_DIR="/var/tmp/aipo/backup"
+#fi
 
 IMAGE_NAME="s6464/my-docker-platform_installer"
 
-mkdir --verbose --parents $AIPO_BACKUP_DIR
-chmod 777 $AIPO_BACKUP_DIR
+#mkdir --verbose --parents $AIPO_BACKUP_DIR
+#chmod 777 $AIPO_BACKUP_DIR
 
 docker build -t $IMAGE_NAME .
-docker run -it --rm -e DOCKER_HOST=$DOCKER_URL -e DOCKER_TLS_VERIFY=$DOCKER_TLS_VERIFY -e DOCKER_HOST_CERT_PATH=$DOCKER_CERT_PATH -e DOCKER_HOST_NGINX_CONF="$DOCKER_HOST_NGINX_CONF" -e DNSMASQ_CONFIG_DIR="$DNSMASQ_CONFIG_DIR" -e AIPO_BACKUP_DIR="$AIPO_BACKUP_DIR" -v $DOCKER_CERT_PATH:/opt/from_dockerfile/certs $IMAGE_NAME
+docker run -it --rm -e DOCKER_HOST=$DOCKER_URL -e DOCKER_TLS_VERIFY=$DOCKER_TLS_VERIFY -e DOCKER_HOST_CERT_PATH=$DOCKER_CERT_PATH -e DOCKER_HOST_NGINX_CONF="$DOCKER_HOST_NGINX_CONF" -e DNSMASQ_CONFIG_DIR="$DNSMASQ_CONFIG_DIR" -v $DOCKER_CERT_PATH:/opt/from_dockerfile/certs $IMAGE_NAME
